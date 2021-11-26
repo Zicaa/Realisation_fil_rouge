edbc326dd4317c83d38e2c2756184696d72ed41b
Zicaa <nausicaa.cresta@gmail.com>
2021-11-26 21:11:33 +0100

    Ajout données BDD

--- Réalisation_4__Conception - MCD, MLD, SQL/BDD MySQL/les_cailloux_de_zicaa.sql
+++ Réalisation_4__Conception - MCD, MLD, SQL/BDD MySQL/les_cailloux_de_zicaa.sql
@@ -151,10 +151,6 @@ quantite_produit int,
 prix_final int
 );
 
-alter table commande
-add constraint fk_commande_id_client foreign key (id_client)
-references commande(id_commande);
-
 create table paiement(
 id_paiement int auto_increment primary key,
 paiement_cb varchar (50),
@@ -214,3 +210,14 @@ id_produit int,
    id_image INT,
    PRIMARY KEY(id_produit, id_categorie, id_image)
 );
+
+use les_cailloux_de_zicaa;
+insert into administrateur (id_admin, nom_admin, prenom_admin, email_admin, adresse_admin, CP_admin, ville_admin) values
+("1","cresta", "zicaa","zikaha@hotmail.fr","16 rue condorcet","34110","frontignan");
+
+insert into client (id_client, id_adresse_livraison, nom_client, prenom_client, email_client, telephone_client, adresse_client, CP_client, ville_client) values
+("2", "20","dupont", "martin","dupont@hotmail.fr","0700000000","15 rue duclient","34000","montpellier"),
+("3", "30","tartampion", "toto","toto@hotmail.fr","0600000000","15 rue delarueinvente","34000","montpellier");
+
+insert into commande (id_commande, id_client, id_produit, nom_client, prenom_client, adresse_client, CP_client, ville_client, montant_commande, quantite_produit, prix_final) values
+("2","2", "12", "dupont", "martin","15 rue duclient","34000","montpellier","15","1","20")

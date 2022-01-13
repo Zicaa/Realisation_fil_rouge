0f5ba79776e5448365054b02bcec04a1ed21dc08
Zicaa <90777736+Zicaa@users.noreply.github.com>
2022-01-13 20:17:26 +0100

    ajout_carousel

---
+++ Réalisation_2__HTML  CSS/Fil_rouge/carousel.js
@@ -0,0 +1,33 @@
+const items = document.querySelectorAll("img");
+const nbSlide = items.length;
+const suivant = document.querySelector(".right");
+const precedent = document.querySelector(".left");
+let compteur = 16;
+
+function slideSuivante(){
+    items[compteur].classList.remove("active");
+
+    if(compteur < nbSlide-1){
+        compteur++;
+    }else{
+        compteur = 0;
+    }
+
+    items[compteur].classList.add("active")
+    console.log(compteur);
+}
+suivant.addEventListener("click", slideSuivante)
+
+function slidePrecedente(){
+    items[compteur].classList.remove("active");
+
+    if(compteur >0){
+        compteur--;
+    }else{
+        compteur = nbSlide -1;
+    }
+
+    items[compteur].classList.add("active")
+    console.log(compteur);
+}
+precedent.addEventListener("click", slidePrecedente)

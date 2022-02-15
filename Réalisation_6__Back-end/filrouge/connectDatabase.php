391a2872b303ea7fd708ee3d212d3fefde06676c
Zicaa <90777736+Zicaa@users.noreply.github.com>
2022-02-15 17:11:53 +0100

    Mise à jour connexion au compte + pwp

--- Réalisation_6__Back-end/filrouge/connectDatabase.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $database= new PDO('mysql:host=localhost;dbname=les_cailloux_de_zicaa', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-    $database->exec("set names utf8"); 
-
-?>

13a6840bbab93c4b1e8aa44006d2983d14bc9a8b
Zicaa <90777736+Zicaa@users.noreply.github.com>
2022-05-18 19:00:16 +0200

    carrousel.js sans commentaires

--- Réalisation_6__Back-end/filrouge/carrousel.js
+++ Réalisation_6__Back-end/filrouge/carrousel.js
@@ -1,48 +1,33 @@
-// je récupère les différents éléments de la page
 const items = document.getElementsByClassName("img");
 const nbSlide = items.length;
 const suivant = document.querySelector(".right");
 const precedent = document.querySelector(".left");
 
-// j'initialise un système de compteur d'image à zéro
 let compteur = 0;
 
-// je crée une fonction qui incrémente chaque image de 1
 function slideSuivante(){
-    // je sélectionne la première image de mon tableau et lui enlève la classe active : elle disparait
     items[compteur].classList.remove("active");
 
-    // je fais apparaitre l'image suivante : si mon compteur est inférieur à -1, j'incrémente de 1
     if(compteur < nbSlide-1){
         console.log(compteur++);
-    // sinon on repasse le compteur à zéro de sorte que la classe active ne s'applique qu'à compteur=0
     }else{
         compteur = 0;
     }
-
-    // j'ajoute la classe active à cette image pour la faire apparaitre
     items[compteur].classList.add("active")
     console.log(compteur);
 }
-// j'ajoute un écouteur d'évènement qui déclenche la fonction au click
 suivant.addEventListener("click", slideSuivante)
 
-// je crée ma fonction qui décrémente chaque image de 1
 function slidePrecedente(){
-    // je sélectionne la première image de mon tableau et lui enlève la classe active : elle disparait
     items[compteur].classList.remove("active");
 
-    // je fais apparaitre l'image précédente : si mon compteur est supérieur à 0, je décrémente de 1
     if(compteur >0){
         compteur--;
-    // sinon on repasse le compteur à zéro de sorte que la classe active ne s'applique qu'à compteur=0
     }else{
         compteur = nbSlide -1;
     }
 
-    // j'ajoute la classe active à cette image pour la faire apparaitre
     items[compteur].classList.add("active")
     console.log(compteur);
 }
-// j'ajoute un écouteur d'évènement qui déclenche la fonction au click
 precedent.addEventListener("click", slidePrecedente)

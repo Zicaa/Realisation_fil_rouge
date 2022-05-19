7ef6e0b27c26a10189a63c8253ed8ee092e4219e
Zicaa <90777736+Zicaa@users.noreply.github.com>
2022-05-19 16:00:57 +0200

    up

--- Réalisation_2__HTML  CSS/filrouge/carrousel.js
+++ Réalisation_2__HTML  CSS/filrouge/carrousel.js
@@ -1,48 +1,43 @@
-// Je récupère les différents éléments de la page
 const items = document.getElementsByClassName("img");
-const nbSlide = items.length;
+const nbSlide = items.length; // tableau qui contient les img
 const suivant = document.querySelector(".right");
 const precedent = document.querySelector(".left");
 
-// j'initialise un système de compteur d'image à zéro
+// système de compteur d'image initialisé à zéro
 let compteur = 0;
 
-// Je crée une fonction qui incrémente chaque image de 1
+// fonction qui incrémente chaque image de 1
 function slideSuivante(){
-    // je sélectionne la première image de mon tableau et lui enlève la classe active : elle disparait
+    // sélection de la première image du tableau : retrait de la classe active
     items[compteur].classList.remove("active");
 
-    // je fais apparaitre l'image suivante : si mon compteur est inférieur à -1, j'incrémente de 1
+    // on incrémente de 1 si le compteur est <-1
     if(compteur < nbSlide-1){
-        console.log(compteur++);
-    // sinon on repasse le compteur à zéro de sorte que la classe active ne s'applique qu'à compteur=0
+        compteur++;
     }else{
         compteur = 0;
     }
 
-    // j'ajoute la classe active à cette image pour la faire apparaitre
+    // ajout de la classe active : l'image apparaît
     items[compteur].classList.add("active")
-    console.log(compteur);
 }
-// j'ajoute un écouteur d'évènement qui déclenche la fonction au click
 suivant.addEventListener("click", slideSuivante)
 
-// Je crée ma fonction qui décrémente chaque image de 1
+// fonction qui décrémente chaque image de 1
 function slidePrecedente(){
-    // je sélectionne la première image de mon tableau et lui enlève la classe active : elle disparait
+    // sélection de la première image du tableau : retrait de la classe active
     items[compteur].classList.remove("active");
 
-    // je fais apparaitre l'image précédente : si mon compteur est supérieur à 0, je décrémente de 1
+    // on décrémente de 1 si le compteur est >0
     if(compteur >0){
         compteur--;
-    // sinon on repasse le compteur à zéro de sorte que la classe active ne s'applique qu'à compteur=0
+    // sinon on recule le tableau d'image de 1
     }else{
         compteur = nbSlide -1;
     }
 
-    // j'ajoute la classe active à cette image pour la faire apparaitre
+    // ajout de la classe active : l'image apparaît
     items[compteur].classList.add("active")
     console.log(compteur);
 }
-// j'ajoute un écouteur d'évènement qui déclenche la fonction au click
 precedent.addEventListener("click", slidePrecedente)

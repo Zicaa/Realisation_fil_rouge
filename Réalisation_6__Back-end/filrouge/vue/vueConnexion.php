200a37e4217d7f7cbf1333c8fc7dafd7dc54a964
Zicaa <90777736+Zicaa@users.noreply.github.com>
2022-05-19 14:38:52 +0200

    up

--- Réalisation_6__Back-end/filrouge/vue/vueConnexion.php
+++ Réalisation_6__Back-end/filrouge/vue/vueConnexion.php
@@ -1,8 +1,8 @@
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8"></meta>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     <title>Connexion</title>
     <link rel="stylesheet" href="formulaire_inscription.css"></link>
 </head>
